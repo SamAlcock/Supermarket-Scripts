168a8b7d82a5622b0b32bb5673fc93fdf8de6030
SamAlcock <72557851+SamAlcock@users.noreply.github.com>
2023-03-14 13:39:24 +0000

    Added player movement and looking

--- ItemHandling.cs
+++ ItemHandling.cs
@@ -18,7 +18,7 @@ public class ItemHandling : MonoBehaviour
     }
     double CalculateItemNumberDisplay(MeshRenderer shelf_renderer, MeshRenderer item_renderer)
     {
-        float shelf_size = shelf_renderer.bounds.size.x - 0.15f; // get shelf width (subtracting to avoid overhang)
+        float shelf_size = shelf_renderer.bounds.size.x - 0.05f; // get shelf width (subtracting to avoid overhang)
         float item_size = item_renderer.bounds.size.x; // get item width
 
         double number_to_loop = System.Math.Floor(shelf_size / item_size); // divide shelf width by item width to get number of items that fit on shelf, then round down to avoid floating items
@@ -49,21 +49,17 @@ public class ItemHandling : MonoBehaviour
         MeshRenderer renderer = item.GetComponent<MeshRenderer>();
 
         float original_y = shelf.transform.position.y; // to help with readability
-        float[] y_offsets = { original_y + 1.565f, original_y + 1.12f, original_y + 0.66f, original_y + 0.2f }; // offsets for each shelf row
-        
+        float[] y_offsets = { original_y + 1.565f, original_y + 1.12f, original_y + 0.66f, original_y + 0.2f }; // offsets for each shelf row 
         float x_gap = renderer.bounds.size.x;
-
         float offset = GetOffset(renderer);
-
         int shelves = 4;
-
         double loop = CalculateItemNumberDisplay(shelf_renderer, renderer);
-
         float rotation = GetRotation(shelf);
+
         Vector3 gap = new();
         Vector3 item_rotation = new();
 
-        if (rotation == 0 || rotation == 360)
+        if (rotation == 0 || rotation == 360) // used to put items across shelf on z axis
         {
             gap = new(0, 0, x_gap);
             item_rotation = new(0, 90, 0);
@@ -78,6 +74,11 @@ public class ItemHandling : MonoBehaviour
             shelf.transform.position += offsets;
         }
 
+        if(rotation == 90 || rotation == 0)
+        {
+            item_rotation = new(0, shelf.transform.eulerAngles.y + 90, 0);
+        }
+
         for (int i = 0; i < shelves; i++)
         {
             Vector3 position = new Vector3(shelf.transform.position.x, shelf.transform.position.y + y_offsets[i], shelf.transform.position.z); // get starting coordiantes of shelf row
@@ -85,7 +86,7 @@ public class ItemHandling : MonoBehaviour
             for (int j = 0; j < loop; j++)
             {
                 Instantiate(item, position, Quaternion.Euler(item_rotation)); // create instance of current item
-                item.transform.Rotate(0, rotation, 0, Space.Self);
+                item.transform.Rotate(0, rotation, 0, Space.Self); // may not need this
                 position -= gap; // decrement by gap
 
             }
---
+++ PlayerMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float walkSpeed = 0.5f;
+
+    Vector2 moveInput;
+    Rigidbody myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        Vector3 playerVelocity = new Vector3(moveInput.x * walkSpeed, myRigidBody.velocity.y, moveInput.y * walkSpeed);
+        myRigidBody.velocity = transform.TransformDirection(playerVelocity);
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+}
